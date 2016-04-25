b2148cd35424d4fc115eecc345fb135058f56094
Beessie <Beessie@users.noreply.github.com>
2016-04-25 20:49:52 +0700

    #02
    create Background and class GUI game for GamePanel
    create class Player and add code From Main committed#01

--- Main.java
+++ Main.java
@@ -8,9 +8,14 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
-		GamePanel gp = new GamePanel();//gui ของเกมส์ เช่น สีพื้นหลัง
+
+		SpaceShip v = new SpaceShip(180, 550, 30, 30); //สร้างผู้เล่น ส่วนที่เขียนเพิ่มจากไฟล์ #1 Main
+		GamePanel gp = new GamePanel();//gui ของเกมส์ทั้งหมด เช่น สีพื้นหลัง สีอักษรแสดงคะแนน
+		GameEngine engine = new GameEngine(gp, v); //ใส่รายละเอียดของเกมส์ทั้งหมด ส่วนที่เขียนเพิ่มจากไฟล์ #1 Main
+		frame.addKeyListener(engine); //ส่วนที่เขียนเพิ่มจากไฟล์ #1 Main
 		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
 		
+		engine.start();//ส่วนที่เขียนเพิ่มจากไฟล์ #1 Main
 	}
 }
