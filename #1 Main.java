71b15df1dc0073facc142671db65fdb6a18268d6
Beessie <Beessie@users.noreply.github.com>
2016-04-25 10:53:55 +0700

    Update #1 Main.java

--- #1 Main.java
+++ #1 Main.java
@@ -8,5 +8,9 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
+		GamePanel gp = new GamePanel();//gui ของเกมส์ เช่น สีพื้นหลัง
+		frame.getContentPane().add(gp, BorderLayout.CENTER);
+		frame.setVisible(true);
+		
 	}
 }
