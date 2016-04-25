8c387078008d2d9501f740e369e159c8508e5582
Beessie <Beessie@users.noreply.github.com>
2016-04-25 10:05:18 +0700

    Update #1 Main.java

--- #1 Main.java
+++ #1 Main.java
@@ -9,13 +9,13 @@ public class Main {
 		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
 
-		SpaceShip v = new SpaceShip(180, 550, 30, 30);
+		/* SpaceShip v = new SpaceShip(180, 550, 30, 30);
 		GamePanel gp = new GamePanel();
 		GameEngine engine = new GameEngine(gp, v);
 		frame.addKeyListener(engine);
 		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
 		
-		engine.start();
+		engine.start();*/
 	}
 }
