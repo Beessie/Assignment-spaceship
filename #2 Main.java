1e0a75d2132b381caaf52c4b4a9e2672ac5aaa09
Beessie <Beessie@users.noreply.github.com>
2016-04-25 20:27:29 +0700

    #02
    create player

--- #2 Main.java
+++ #2 Main.java
@@ -9,13 +9,13 @@ public class Main {
 		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
 
-		SpaceShip v = new SpaceShip(180, 550, 30, 30); //สร้างผู้เล่น ส่วนที่เขียนเพิ่มจากไฟล์ #1 Main
+		SpaceShip v = new SpaceShip(180, 550, 30, 30); //สร้างผู้เล่น ส่วนที่เขียนเพิ่ม
 		GamePanel gp = new GamePanel();//gui ของเกมส์ทั้งหมด เช่น สีพื้นหลัง สีอักษรแสดงคะแนน
-		GameEngine engine = new GameEngine(gp, v); //ใส่รายละเอียดของเกมส์ทั้งหมด ส่วนที่เขียนเพิ่มจากไฟล์ #1 Main
-		frame.addKeyListener(engine); //ส่วนที่เขียนเพิ่มจากไฟล์ #1 Main
+		GameEngine engine = new GameEngine(gp, v); //ใส่รายละเอียดของเกมส์ทั้งหมด ส่วนที่เขียนเพิ่ม
+		frame.addKeyListener(engine); //ส่วนที่เขียนเพิ่ม
 		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
 		
-		engine.start();//ส่วนที่เขียนเพิ่มจากไฟล์ #1 Main
+		engine.start();//ส่วนที่เขียนเพิ่ม
 	}
 }
